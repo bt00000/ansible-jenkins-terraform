b175df2abef0cf2f506b8949aad1437b3d5f9710
bt00000 <bobthecat1020@gmail.com>
2025-03-15 13:37:37 -0700

    Add VPC, Outbound rule to security group, Route Table, Subnet

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_key_pair" "deployer" {
 
 resource "aws_security_group" "jenkins_sg" {
   name_prefix = "jenkins-sg-"
+  vpc_id      = aws_vpc.jenkins_vpc.id # Attach security group to the new VPC
 
   ingress {
     from_port   = 22
@@ -30,13 +31,72 @@ resource "aws_security_group" "jenkins_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allow all protocols
+    cidr_blocks = ["0.0.0.0/0"]  # Allow all destinations
+  }
 }
 
+# Create a VPC
+resource "aws_vpc" "jenkins_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Jenkins-VPC"
+  }
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "jenkins_igw" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+
+  tags = {
+    Name = "Jenkins-Internet-Gateway"
+  }
+}
+
+# Create a Public Subnet
+resource "aws_subnet" "jenkins_subnet" {
+  vpc_id                  = aws_vpc.jenkins_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Jenkins-Public-Subnet"
+  }
+}
+
+# Create a Route Table
+resource "aws_route_table" "jenkins_route_table" {
+  vpc_id = aws_vpc.jenkins_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.jenkins_igw.id
+  }
+
+  tags = {
+    Name = "Jenkins-Route-Table"
+  }
+}
+
+# Associate the Route Table with the Subnet
+resource "aws_route_table_association" "jenkins_rta" {
+  subnet_id      = aws_subnet.jenkins_subnet.id
+  route_table_id = aws_route_table.jenkins_route_table.id
+}
+
+# Update EC2 Instance with Correct Subnet
 resource "aws_instance" "jenkins_instance" {
-  ami                    = var.ami_id
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  ami                         = var.ami_id
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
+  subnet_id                   = aws_subnet.jenkins_subnet.id
+  associate_public_ip_address = true
 
   tags = {
     Name = "Jenkins-Server"
@@ -46,3 +106,5 @@ resource "aws_instance" "jenkins_instance" {
 output "instance_public_ip" {
   value = aws_instance.jenkins_instance.public_ip
 }
+
+
